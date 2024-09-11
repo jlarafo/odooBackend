37fd95a0b2a72ba26310b214e2f764657d2c2308
jlara <jlaramintic@gmail.com>
2024-09-11 11:30:44 -0500

    up ngrok

--- src/controllers/adquirientes.controller.js
+++ src/controllers/adquirientes.controller.js
@@ -50,8 +50,8 @@ export const createAdquiriente = async (req, res) => {
 
         // Configuración de la solicitud HTTP para /mail
         const mailOptions = {
-            hostname: '6893-2800-484-788f-d600-d956-9a8a-f3bb-38b6.ngrok-free.app',
-            //hostname: '192.168.0.19',
+            //hostname: '6893-2800-484-788f-d600-d956-9a8a-f3bb-38b6.ngrok-free.app',
+            hostname: '0430-2800-484-788f-d600-6849-c465-cfeb-fafb.ngrok-free.app ',
             //port: 5000,
             path: '/mail',
             method: 'POST',
@@ -68,8 +68,8 @@ export const createAdquiriente = async (req, res) => {
                 setTimeout(() => {
                     // Configuración de la solicitud HTTP para /crearadquiriente
                     const adquirienteOptions = {
-                        hostname: '6893-2800-484-788f-d600-d956-9a8a-f3bb-38b6.ngrok-free.app',
-                        //hostname: '192.168.0.19',
+                        //hostname: '6893-2800-484-788f-d600-d956-9a8a-f3bb-38b6.ngrok-free.app',
+                        hostname: '0430-2800-484-788f-d600-6849-c465-cfeb-fafb.ngrok-free.app ',
                         //port: 5000,
                         path: '/crearadquiriente',
                         method: 'POST',
