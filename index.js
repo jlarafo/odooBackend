af33ef84c14d94715fc50e15fe044cc5ed9e1ec7
jlara <jlaramintic@gmail.com>
2024-09-11 11:58:12 -0500

    up ip

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import app from "./src/app.js";
 import { PORT } from "./src/config.js";
 
 app.listen(PORT);
-console.log(`Server on port http://192.168.0.19:${PORT}`);
+console.log(`Server on port http://localhost:${PORT}`);
